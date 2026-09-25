e38ac280b84ade6fefb47256b588de33e0328903
Oskar Orozco <oskarozco@gmail.com>
2026-09-25 01:21:03 -0500

    commit inicial

---
+++ src/main/java/com/nuevaeps/api_auth/ApiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nuevaeps.api_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthApplication.class, args);
+	}
+
+}
